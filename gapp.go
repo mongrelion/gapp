617b09e4682d9b145aaedda0134496b0740eb622
Carlos León <mail@carlosleon.info>
2016-05-02 11:27:58 +0200

    Read version and bind iface from ENV.

--- gapp.go
+++ gapp.go
@@ -4,12 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
+
+var version string
+var bind string
+
+func init() {
+	version = os.Getenv("VERSION")
+	if len(version) == 0 {
+		version = "unknown version"
+	}
+
+	bind = os.Getenv("BIND")
+	if len(bind) == 0 {
+		bind = ":8080"
+	}
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "ok")
+		fmt.Fprintf(w, version)
 	})
-	fmt.Println("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Printf("Binding to %s\n", bind)
+	fmt.Printf("Running version %s\n", version)
+	log.Fatal(http.ListenAndServe(bind, nil))
 }
