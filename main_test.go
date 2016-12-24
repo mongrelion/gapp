893f61f2fe962e0f873257be5d85fbd597bdb912
Carlos León <mail@carlosleon.info>
2016-12-24 13:34:55 +0100

    Fix tests

--- main_test.go
+++ main_test.go
@@ -1,8 +1,41 @@
 package main
 
 import (
+	"flag"
+	"log"
+	"os"
 	"testing"
 )
+
+const TEST_VERSION = "1.0"
+
+func setup() error {
+	err := os.Setenv("VERSION", TEST_VERSION)
+	return err
+}
+
+func teardown() error {
+	err := os.Unsetenv("VERSION")
+	return err
+}
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	/* Perform extra setup before running tests */
+	if err := setup(); err != nil {
+		log.Fatal(err) // logs out the error and calls os.Exit(1)
+	}
+
+	out := m.Run()
+
+	/* Perform tear down after running tests */
+	if err := teardown(); err != nil {
+		log.Fatal(err) // logs out the error and calls os.Exit(1)
+	}
+
+	os.Exit(out)
+}
 
 func TestGetVersion(t *testing.T) {
 	v := GetVersion()
